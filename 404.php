62e5da0d72cff748de75560274e54c58838e2bfb
Md. Ashraful Mollah <ashrafulash@github.com>
2020-04-04 15:30:31 +0600

    404 page responsive

--- 404.php
+++ 404.php
@@ -1,4 +1,7 @@
 <?php wp_head(); ?>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+</head>
 <body <?php body_class(); ?>>
 
     <div class="error-page">
