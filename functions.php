1e550579c61b18f48d5d0725e693abe21d543ca3
Md. Ashraful Mollah <ashrafulash@github.com>
2020-04-07 18:17:12 +0600

    blog single added

--- functions.php
+++ functions.php
@@ -59,11 +59,37 @@ function wpdocs_excerpt_more( $more ) {
 }
 add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
+// views list in blog single page
+function getPostViews($postID){
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 View";
+    }
+    return $count.' Views';
+}
+function setPostViews($postID) {
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+// Remove issues with prefetching adding extra views
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
 // add navigation
 register_nav_menus([
     'default-menu' => esc_html__('Default Menu', 'invitationcentre'),
 ]);
+
 
 
 // add widgets area
--- single.php
+++ single.php
@@ -4,14 +4,68 @@
         <?php get_template_part('template/sidebar'); ?>
     </div>
     <div class="col-lg-9 mx-auto maincontent page-default" id="mx-100">
-        <?php if(have_posts()) :  while(have_posts()) :  the_post();
-        ?>
+        <?php if(have_posts()) : the_post(); ?>
+        <div class="single-page">
+            <div class="single-page__banner">
+                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+
+                <div class="single-page__banner__details">
+                    <span class="entry-date"><i class="far fa-clock"></i> <?php the_date(); ?></span>
+                    <span class="entry-date"><?php the_date(); ?></span>
+                    <span class="entry-category">
+                        <i class="far fa-folder"></i>
+                        <?php 
+                            foreach((get_the_category()) as $category){
+                                echo $category->name;
+                            }	
+                        ?>
+                    </span>
+                    <span class="entry-author"><i class="far fa-user-circle"></i> <?php the_author() ?></span>
+
+                    <span><i class="far fa-eye"></i> <?php echo getPostViews(get_the_ID());?></span>
+
+                </div>
+
+                <div class="single-page__banner__thumbnail">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6">
+                            <div class="img">
+                                <?php
+                                if ( has_post_thumbnail() ) :
+                                    the_post_thumbnail();
+
+                                else:
+                                ?>
+                                    <img class="w-100 wp-post-image" src="<?php echo get_template_directory_uri() ?>/asset/src/images/default-blog-image.jpg" alt="<?php the_title() ?>">
+                                
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                            <div class="excerpt">
+                                <?php 
+                                    the_excerpt('<p>','</p>');
+                                ?>
+                                <?php 
+                                    echo do_shortcode( ' [xs_social_share] ' );
+                                ?>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        
         
         <?php the_content('<div>', '</div>'); ?>
 
-        <?php endwhile; else : ?>
+        <?php else : ?>
             <h1>no post found</h1>
+        </div>
         <?php endif; ?>
+
+        
         <!-- inner footer -->
         <?php get_template_part('template/inner-footer'); ?>
 
